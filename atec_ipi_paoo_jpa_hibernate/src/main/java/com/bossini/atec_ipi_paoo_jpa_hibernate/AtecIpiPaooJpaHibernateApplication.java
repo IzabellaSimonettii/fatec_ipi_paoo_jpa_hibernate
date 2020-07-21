fdd2a75545306c09c7d9a4057bfa112289063355
Izabella Chianca Simonetti <izabellayeh@gmail.com>
2020-07-20 22:24:57 -0300

    Aula 20/07/20

---
+++ atec_ipi_paoo_jpa_hibernate/src/main/java/com/bossini/atec_ipi_paoo_jpa_hibernate/AtecIpiPaooJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.bossini.atec_ipi_paoo_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtecIpiPaooJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtecIpiPaooJpaHibernateApplication.class, args);
+	}
+
+}
